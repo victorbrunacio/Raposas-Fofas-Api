dae3e293bc9629d3343c23beda734a13518dd518
Victor Brunacio <afterisbest@gmail.com>
2022-06-01 18:49:48 -0300

    Upando site

---
+++ teste.js
@@ -0,0 +1,17 @@
+const buttonRposa = document.getElementById("change-raposas")
+const imagemRaposa = document.getElementById("raposas")
+
+const api = "https://randomfox.ca/floof/?ref=apilist.fun"
+
+async function pegaImg () {
+    const apiJson = await fetch(api)
+    .then((x)=> x.json())
+    .catch((err)=>{
+        console.log(err)
+    })
+    return apiJson.image
+}
+
+async function trocaRaposa() {
+    imagemRaposa.src = await pegaImg()
+}
